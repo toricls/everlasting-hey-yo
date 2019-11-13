5621ff405c860696f452c9085ec144e8188c331e
toricls <toriclstr@gmail.com>
2019-11-13 16:46:10 +0900

    Now we have log message patterns!

--- hey-yo.sh
+++ hey-yo.sh
@@ -1,20 +1,30 @@
 #!/bin/sh
 
+MSG="Hey, Yo!"
+
 trap IWillNeverDie 15
 
 IWillNeverDie()
 {
   if [ -z "${LET_ME_DIE}" ]; then
-    echo "Hey, Hey, Hey, Yo!!!"
-  else
     echo "Hey, he..."
     sleep 5
     exit 0
-  fi  
+  else
+    echo "Hey, Hey, ${MSG}!!"
+  fi
 }
 
 while true
 do
-  echo "Hey, Yo!"
+  if [ -z "${GIVE_ME_PATTERNS}" ]; then
+    echo "${MSG}"
+  else
+    if [ $((${RANDOM} % 2)) = 1 ]; then
+      echo "A lot of ${MSG}"
+    else
+      echo "${MSG}"
+    fi
+  fi
   sleep 1
 done
